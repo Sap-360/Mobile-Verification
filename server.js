99172928d64f2994b78acbdbfb08659d65d7ec05
lovingyugs <lovingyugs95@gmail.com>
2017-05-02 15:04:11 +0530

    updated for hekorku

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var app = express(),
 	config=require('./config'),
 	bodyParser=require('body-parser');
 
+var autho = require("node-autho");
+
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
@@ -18,6 +20,32 @@ app.use(express.static(__dirname + '/public'));
 app.get('*',function(req,res){
     res.sendFile(__dirname +'/public/index.html'); 
 })
+
+autho.certPath = "./node_modules/node-autho/cert.txt";
+console.log(autho.certPath);
+
+var gen;
+autho.encryptPassword("the quick brown fox").then(function(hash){
+  // Save to a database
+  console.log(hash);
+   autho.comparePassword("the quick brown fox",hash).then(function(){
+          // Comparsion passed
+          console.log("passed");
+        },
+        function(){
+            console.log("failed");
+          // Comparison failed
+        });
+});
+
+autho.decrypt("d38c0b6d319112f661e0d5ef57114a7c60c6ad74f0d465f7f3ac158f64968a65").then(function(res){
+    console.log(typeof(res));
+
+           
+});
+
+
+
 
 app.listen(config.port, function (err) {
 	if(err){
